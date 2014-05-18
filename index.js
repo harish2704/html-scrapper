a26d4542f02cb7e8bbaf92fcc7a30b17d180c5ba
Harish.K <harish2704@gmail.com>
2014-05-18 17:48:46 +0530

    Added Index.js Rm unwanted imports Fixed package.json syntax

---
+++ index.js
@@ -0,0 +1,9 @@
+var dataSource = require('./lib/data-source');
+var dataExtractor = require('./lib/data-extractor');
+var dataFetcher = require('./lib/data-fetcher');
+
+exports.Source = dataSource.Source;
+exports.Extractor = dataExtractor.Extractor;
+exports.Fetcher = dataFetcher.Fetcher;
+
+
