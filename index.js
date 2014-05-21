31c7e840265e5dc3c9cdd51fc6109a0480accf71
Harish.K <harish2704@gmail.com>
2014-05-21 11:49:04 +0530

    Move extraction functions to seperate file. added some general purpose functions.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var dataSource = require('./lib/data-source');
 var dataExtractor = require('./lib/data-extractor');
 var dataFetcher = require('./lib/data-fetcher');
+var fn = require('./lib/fn');
 
 exports.Source = dataSource.Source;
 exports.Extractor = dataExtractor.Extractor;
 exports.Fetcher = dataFetcher.Fetcher;
+exports.Fn = fn;
 
 
